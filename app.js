a4f3fa7ef44fa02db870e87f54d9c9bb11a9defa
genaro rafault <genarorafault@gmail.com>
2023-03-01 10:27:52 -0300

    Proyecto

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use( express.static(publicPath) );
+
+app.listen(5000, () => {
+    console.log("EXITO");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+    
+
+
